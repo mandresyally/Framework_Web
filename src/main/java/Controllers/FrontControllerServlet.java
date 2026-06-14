bc9a5bd0db1e98855a321ee4dcdce3138f193f43
RepoUser <you@example.com>
2026-06-14 15:11:56 +0300

    15:11

--- src/main/java/Controllers/FrontControllerServlet.java
+++ src/main/java/Controllers/FrontControllerServlet.java
@@ -11,6 +11,12 @@ import jakarta.servlet.http.HttpServletResponse;
 public class FrontControllerServlet extends HttpServlet {
  protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+
+
+
+
+
+    
    response.setContentType("text/html;charset=UTF-8");
     try(PrintWriter out = response.getWriter()) {
             String uri = request.getRequestURI();
